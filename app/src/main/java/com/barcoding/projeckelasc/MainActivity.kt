5a55278db2b0a5ecda8b8409dac6766d02b8d527
LuthfiRafif <luthfi0599@gmail.com>
2020-04-24 15:36:42 +0800

    Implementasi Toast pada OnClick TextView

--- app/src/main/java/com/barcoding/projeckelasc/MainActivity.kt
+++ app/src/main/java/com/barcoding/projeckelasc/MainActivity.kt
@@ -2,11 +2,22 @@ package com.barcoding.projeckelasc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val TextView = findViewById<TextView>(R.id.tv_1)
+
+        tv_1.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"Hai", Toast.LENGTH_LONG).show()
+        })
+
     }
 }
